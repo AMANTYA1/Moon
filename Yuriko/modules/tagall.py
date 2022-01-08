e084c019c8270fa6c8f497dcc57aff45e1179451
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-01-08 15:36:22 +0530

    Update tagall.py

--- Yuriko/modules/tagall.py
+++ Yuriko/modules/tagall.py
@@ -117,7 +117,7 @@ Yuriko Can Be a Mention Bot for your group.
 
 Only admins can tag all.  here is a list of commands
 
-✗ /tagall or @all '(reply to message or add another message)' 'To mention all members in your group, without exception.'
+✗ /tagall or @all '(reply to message or add another message) To mention all members in your group, without exception.'
 
 *✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
 """
