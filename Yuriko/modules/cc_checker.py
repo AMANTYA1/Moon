4b422e74e41b563f35c07b78505ea88bbdc7426e
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-02-03 21:39:13 +0530

    Update cc_checker.py

--- Yuriko/modules/cc_checker.py
+++ Yuriko/modules/cc_checker.py
@@ -260,19 +260,19 @@ async def alive(event):
             await k.edit(text)
 
 
-__mod_name__ = "CC CHEKER"
+__mod_name__ = "Cᴄ-Cʜᴇᴄᴋʜᴇʀ"
 
 __help__ = """
 Help for CC Checker Module:
 
 Available Commands:
-- /au [cc]: Stripe Auth given CC
-- /pp [cc]: Paypal 1$ Guest Charge
-- /ss [cc]: Speedy Stripe Auth
-- /ch [cc]: Check If CC is Live
-- /bin [bin]: Gather's Info About the bin
-- /gen [bin]: Generates CC with given bin
-- /key [sk]: Checks if Stripe key is Live
+✗ /au `[cc]: Stripe Auth given CC`
+✗ /pp `[cc]: Paypal 1$ Guest Charge`
+✗ /ss `[cc]: Speedy Stripe Auth`
+✗ /ch `[cc]: Check If CC is Live`
+✗ /bin `[bin]: Gather's Info About the bin`
+✗ /gen `[bin]: Generates CC with given bin`
+✗ /key `[sk]: Checks if Stripe key is Live`
 
 Note: Format of cc is ccnum|mm|yy|cvv
     Privacy warning: Don't check any of your personal CC's.
