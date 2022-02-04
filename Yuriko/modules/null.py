cff32a1d6b0ab9e94dbed626c04f562919f5ba3e
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-02-05 03:53:02 +0530

    Update null.py

--- Yuriko/modules/null.py
+++ Yuriko/modules/null.py
@@ -1,19 +1,10 @@
-__mod_name__ = "ǫʀ Cᴏᴅᴇ"
+__mod_name__ = "Qʀ Cᴏᴅᴇ"
 
 __help__ = """
-Help for CC Checker Module:
+Commands
+✗ /getqr - `Get text in qr.`
 
-Available Commands:
-✗ /au `[cc]: Stripe Auth given CC`
-✗ /pp `[cc]: Paypal 1$ Guest Charge`
-✗ /ss `[cc]: Speedy Stripe Auth`
-✗ /ch `[cc]: Check If CC is Live`
-✗ /bin `[bin]: Gather's Info About the bin`
-✗ /gen `[bin]: Generates CC with given bin`
-✗ /key `[sk]: Checks if Stripe key is Live`
-
-Note: Format of cc is ccnum|mm|yy|cvv
-    Privacy warning: Don't check any of your personal CC's.
+✗ /makeqr - `Make a qr code`
 
 *✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
 """
