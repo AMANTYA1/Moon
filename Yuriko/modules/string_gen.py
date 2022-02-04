1ccc5229c5bec5cbcc2efd2c48605063d81fc38e
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-02-05 01:01:44 +0530

    Update string_gen.py

--- Yuriko/modules/string_gen.py
+++ Yuriko/modules/string_gen.py
@@ -1,16 +1,16 @@
-__mod_name__ = "STRING GEN"
+__mod_name__ = "Sᴛʀɪɴɢ-Gᴇɴ"
 
 __help__ = """
 *Yuriko String GENRATOR*
 
 ✨ Available Commands ✨
 
-✗ /about - About The Bot
-✗ /help - This Message
-✗ /start - Start the Bot
-✗ /generate - Start Generating Session
-✗ /cancel - Cancel the process
-✗ /restart - Cancel the process 
+✗ /about - `About The Bot`
+✗ /help - `This Message`
+✗ /start - `Start the Bot`
+✗ /generate - `Start Generating Session|
+✗ /cancel - `Cancel the process|
+✗ /restart - `Cancel the process`
 
 *✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
 """
