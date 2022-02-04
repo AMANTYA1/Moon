ed2004d9d6695aaeaee388d6063ce461669c3e81
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-02-05 02:15:09 +0530

    Create dulltime.py

---
+++ Yuriko/modules/dulltime.py
@@ -0,0 +1,10 @@
+__mod_name__ = "Dᴀᴛᴇ-Tɪᴍᴇ"
+
+__help__ = """
+
+✗ /datetime `[timezone] Get the present date and time information`
+
+`You can check out this` [link](https://timezonedb.com/time-zones)`for the available timezones`
+
+*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
+"""
