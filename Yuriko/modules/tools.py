0b6a662ba124844b0b19ca8c541b40f28fd4a670
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-02-05 14:06:46 +0530

    Update tools.py

--- Yuriko/modules/tools.py
+++ Yuriko/modules/tools.py
@@ -5,16 +5,6 @@ __help__ = """
 
 ✗  /sg <reply> - `To check history name`
 
-*Date-time-Weather*
-
-✗  /time - `<country code> Gives information about a timezone.`
-
-✗  /weather - `<city> Get weather info in a particular place.`
-
-✗  /wttr - `<city> Advanced weather module, usage same as /weather`
-
-✗  /wttr - `moon Get the current status of moon`
-
 *Converts*
 
 ✗  /encrypt - `Encrypts The Given Text`
