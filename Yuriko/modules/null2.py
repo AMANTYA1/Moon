70545adb0fbc32156b4c4cd6489c6c6c85df93a2
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-02-08 16:41:06 +0530

    Create null2.py

---
+++ Yuriko/modules/null2.py
@@ -0,0 +1,22 @@
+__mod_name__ = "Tᴀɢ Aʟᴇʀᴛ"
+
+__help__ = """
+`Too many mentions.. Cant you manage them all alone..`
+
+<b>Here is the solution</b>
+
+`If you are tagged/mentioned in a group where Yuriko is present`
+
+¥Yuriko will notify it to you via private message after enabling tag alerts`
+
+<b>Available Commands: (only work on bot inbox)</b>
+
+✗ /tagalert <code>[on/off]</code> : Enable or Disable tag alerts
+
+Example:
+
+`If you are mentioned in a group Shadow will tell you who mentioned you, message that you are tagged in and which group is that`
+
+**✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!**
+"""
+"""
