b2aa356eb1611325178d887d07d7f9f8bc308f49
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-02-13 16:10:24 +0530

    Update log_channel.py

--- Yuriko/modules/log_channel.py
+++ Yuriko/modules/log_channel.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from functools import wraps
 from telegram.ext import CallbackContext
-from Zaid.modules.helper_funcs.misc import is_module_loaded
+from Yuriko.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
@@ -11,9 +11,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, JobQueue, run_async
     from telegram.utils.helpers import escape_markdown
 
-    from Zaid import EVENT_LOGS, LOGGER, dispatcher
-    from Zaid.modules.helper_funcs.chat_status import user_admin
-    from Zaid.modules.sql import log_channel_sql as sql
+    from Yuriko import EVENT_LOGS, LOGGER, dispatcher
+    from Yuriko.modules.helper_funcs.chat_status import user_admin
+    from Yuriko.modules.sql import log_channel_sql as sql
 
     def loggable(func):
         @wraps(func)
