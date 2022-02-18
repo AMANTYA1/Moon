a3eed1a8c287863e52ff6478af5b1c128ac51d7d
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-02-18 14:59:57 +0530

    Update language.py

--- Yuriko/modules/language.py
+++ Yuriko/modules/language.py
@@ -1,178 +1,76 @@
-# Copyright (C) 2018 - 2020 MrYacha. All rights reserved. Source code available under the AGPL.
-# Copyright (C) 2021 TeamOfShadow
-# Copyright (C) 2020 - 2021 DeshadeethThisarana
+from typing import Union, List, Dict, Callable, Generator, Any
+import itertools
+from collections.abc import Iterable
+from telegram.ext import CommandHandler, CallbackQueryHandler
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 
-# This file is part of Shadow (Telegram Bot)
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-from contextlib import suppress
-
-from aiogram.types.inline_keyboard import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.utils.callback_data import CallbackData
-from aiogram.utils.exceptions import MessageNotModified
-
-from Yuriko.events import register
-from Yuriko.mongo import db
-
-from .utils.language import (
-    LANGUAGES,
-    change_chat_lang,
-    get_chat_lang_info,
-    get_strings,
-    get_strings_dec,
-)
-from .utils.message import get_arg
-
-select_lang_cb = CallbackData("select_lang_cb", "lang", "back_btn")
-translators_lang_cb = CallbackData("translators_lang_cb", "lang")
+from Yuriko import dispatcher
+import Yuriko.modules.sql.language_sql as sql
+from Yuriko.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
+from Yuriko.langs import get_string, get_languages, get_language
 
 
-@register(cmds="lang", no_args=True, user_can_change_info=True)
-async def select_lang_cmd(message):
-    await select_lang_keyboard(message)
 
-
-@get_strings_dec("language")
-async def select_lang_keyboard(message, strings, edit=False):
-    markup = InlineKeyboardMarkup(row_width=2)
-    task = message.reply if edit is False else message.edit_text
-
-    lang_info = await get_chat_lang_info(message.chat.id)
-
-    if message.chat.type == "private":
-        text = strings["your_lang"].format(
-            lang=lang_info["flag"] + " " + lang_info["babel"].display_name
+def paginate(
+    iterable: Iterable, page_size: int
+) -> Generator[List, None, None]:
+    while True:
+        i1, i2 = itertools.tee(iterable)
+        iterable, page = (
+            itertools.islice(i1, page_size, None),
+            list(itertools.islice(i2, page_size)),
         )
-        text += strings["select_pm_lang"]
+        if not page:
+            break
+        yield page
 
-    # TODO: Connected chat lang info
 
-    else:
-        text = strings["chat_lang"].format(
-            lang=lang_info["flag"] + " " + lang_info["babel"].display_name
-        )
-        text += strings["select_chat_lang"]
+def gs(chat_id: Union[int, str], string: str) -> str:
+    lang = sql.get_chat_lang(chat_id)
+    return get_string(lang, string)
 
-    for lang in LANGUAGES.values():
-        lang_info = lang["language_info"]
-        markup.insert(
+
+@user_admin
+def set_lang(update: Update, _) -> None:
+    chat = update.effective_chat
+    msg = update.effective_message
+
+    msg_text = gs(chat.id, "curr_chat_lang").format(
+        get_language(sql.get_chat_lang(chat.id))[:-3]
+    )
+
+    keyb = [InlineKeyboardButton(
+                text=name,
+                callback_data=f"setLang_{code}",
+            ) for code, name in get_languages().items()]
+    keyb = list(paginate(keyb, 2))
+    keyb.append(
+        [
             InlineKeyboardButton(
-                lang_info["flag"] + " " + lang_info["babel"].display_name,
-                callback_data=select_lang_cb.new(
-                    lang=lang_info["code"], back_btn=False if edit is False else True
-                ),
+                text="🆘 Help us in translations",
+                url="https://Github.com/AMANTYA1/MRSERICA",
             )
-        )
-
-    markup.add(
-        InlineKeyboardButton(
-            strings["crowdin_btn"], url="https://t.me/ShadowSupport_Official"
-        )
+        ]
     )
-    if edit:
-        markup.add(InlineKeyboardButton(strings["back"], callback_data="go_to_start"))
-    with suppress(MessageNotModified):
-        await task(text, reply_markup=markup)
+    msg.reply_text(msg_text, reply_markup=InlineKeyboardMarkup(keyb))
 
 
-async def change_lang(message, lang, e=False, back_btn=False):
-    chat_id = message.chat.id
-    await change_chat_lang(chat_id, lang)
+@user_admin_no_reply
+def lang_button(update: Update, _) -> None:
+    query = update.callback_query
+    chat = update.effective_chat
 
-    strings = await get_strings(chat_id, "language")
+    query.answer()
+    lang = query.data.split("_")[1]
+    sql.set_lang(chat.id, lang)
 
-    lang_info = LANGUAGES[lang]["language_info"]
-
-    text = strings["lang_changed"].format(
-        lang_name=lang_info["flag"] + " " + lang_info["babel"].display_name
-    )
-    text += strings["help_us_translate"]
-
-    markup = InlineKeyboardMarkup()
-
-    if "translators" in lang_info:
-        markup.add(
-            InlineKeyboardButton(
-                strings["see_translators"],
-                callback_data=translators_lang_cb.new(lang=lang),
-            )
-        )
-
-    if back_btn == "True":
-        # Callback_data converts boolean to str
-        markup.add(InlineKeyboardButton(strings["back"], callback_data="go_to_start"))
-
-    if e:
-        with suppress(MessageNotModified):
-            await message.edit_text(
-                text, reply_markup=markup, disable_web_page_preview=True
-            )
-    else:
-        await message.reply(text, reply_markup=markup, disable_web_page_preview=True)
-
-
-@register(cmds="lang", has_args=True, user_can_change_info=True)
-@get_strings_dec("language")
-async def select_lang_msg(message, strings):
-    lang = get_arg(message).lower()
-
-    if lang not in LANGUAGES:
-        await message.reply(strings["not_supported_lang"])
-        return
-
-    await change_lang(message, lang)
-
-
-@register(
-    select_lang_cb.filter(),
-    f="cb",
-    allow_kwargs=True,
-)
-async def select_lang_callback(query, callback_data=None, **kwargs):
-    lang = callback_data["lang"]
-    back_btn = callback_data["back_btn"]
-    await change_lang(query.message, lang, e=True, back_btn=back_btn)
-
-
-async def __stats__():
-    return f"• <code>{len(LANGUAGES)}</code> languages loaded.\n"
-
-
-async def __export__(chat_id):
-    lang = await get_chat_lang_info(chat_id)
-
-    return {"language": lang["code"]}
-
-
-async def __import__(chat_id, data):
-    if data not in LANGUAGES:
-        return
-    await db.lang.update_one(
-        {"chat_id": chat_id}, {"$set": {"lang": data}}, upsert=True
+    query.message.edit_text(
+        gs(chat.id, "set_chat_lang").format(get_language(lang)[:-3])
     )
 
 
-__mod_name__ = "Languages"
+SETLANG_HANDLER = CommandHandler("lang", set_lang)
+SETLANG_BUTTON_HANDLER = CallbackQueryHandler(lang_button, pattern=r"setLang_")
 
-__help__ = """
-This module is dedicated towards utilising Yuriko's localization feature! 
-
-<b>Available commands:</b>
-- /lang: Shows a list of avaible languages
-- /lang <code>(language codename)</code>: Sets a language
-
-<b>Example:</b> <code>/lang</code>
-Yuriko will send you bunch of inline buttons where you can select your prefered language interatively without any hassles!
-"""
+dispatcher.add_handler(SETLANG_HANDLER)
+dispatcher.add_handler(SETLANG_BUTTON_HANDLER)
