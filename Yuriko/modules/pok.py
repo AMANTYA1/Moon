060dd62c0bd522dcaa3d8dfe4e94e1d836791e67
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-02-23 16:49:37 +0530

    Create pok.py

---
+++ Yuriko/modules/pok.py
@@ -0,0 +1,10 @@
+__mod_name__ = "Aɴᴛɪ ᴄʜᴀɴɴᴇʟ"
+
+__help__ = """
+Commands
+✗ /antichannel on - `To on antichannel.`
+
+✗ /antichannel off - `To off antichannel`
+
+*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
+"""
