12a558ddb46d590d4ee2a1312fa361a573c4967c
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-02-23 20:57:31 +0530

    Add files via upload

---
+++ Yuriko/modules/mongo/antichnl_mongo.py
@@ -0,0 +1,23 @@
+from Avenger.modules.mongo import anitcdb
+
+async def is_antichnl(group_id):
+    data = await anitcdb.find_one({"group_id": group_id})
+    if not data:
+        return False, None
+    else: 
+        return True, data["mode"]
+
+async def antichnl_on(group_id, mode):
+    data = {
+        "group_id":group_id,
+        "mode":(mode)}
+    try:
+        anitcdb.update_one({"group_id": group_id},  {"$set": data}, upsert=True)
+    except:
+        return
+
+async def antichnl_off(group_id):
+    is_captcha, type = await is_antichnl(group_id)
+    if not is_captcha:
+        return
+    return anitcdb.delete_one({"group_id": group_id})
