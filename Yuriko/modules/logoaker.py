fc6d63d52057e2d89a868925651bbadc419d0156
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-02-27 19:12:42 +0530

    Update logoaker.py

--- Yuriko/modules/logoaker.py
+++ Yuriko/modules/logoaker.py
@@ -6,7 +6,7 @@ from pyrogram import filters
 from Yuriko.function.pluginhelpers import get_text
 from Yuriko import pbot
 
-API1 = "https://api.single-developers.software/logohq?name="
+API1 = "https://single-developers.up.railway.app/logo?name=name"
 API2 = "https://single-developers.up.railway.app/logohq?name=name"
 
 
