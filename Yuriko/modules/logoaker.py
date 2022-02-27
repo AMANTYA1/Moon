0a032810af1762eae4b6aaf03fefcb3c617ba30b
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-02-27 18:55:52 +0530

    Update logoaker.py

--- Yuriko/modules/logoaker.py
+++ Yuriko/modules/logoaker.py
@@ -210,3 +210,16 @@ async def logohq(client, message):
             )
         except:
             return
+
+
+__mod_name__ = "Lᴏɢᴏ"
+
+__help__ = """
+Commands
+✗ /logo - `to get logo.`
+
+✗ /logohq - `to get high quality logo
+
+*✗ Pᴏᴡᴇʀᴇᴅ 🔥 Bʏ: Kɪɢᴏ Dᴜɴɪʏᴀ!*
+"""
+__lovely_tools__ = __help__
