e24682e86b8b9bdfce48ee37ea7694bb8f7bb8d9
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-18 02:38:47 +0530

    Update dbfunctions.py

--- Yuriko/ex_plugins/dbfunctions.py
+++ Yuriko/ex_plugins/dbfunctions.py
@@ -2,7 +2,7 @@ import codecs
 import pickle
 from typing import Dict, List, Union
 
-from Yuriko.mongo import db
+from Yuriko.mono import db
 
 # SOME THINGS ARE FUCKED UP HERE, LIKE TOGGLEABLES HAVE THEIR OWN COLLECTION
 # (SHOULD FIX IT WITH SOMETHING LIKE TOGGLEDB), MOST OF THE CODE IS BAD AF
