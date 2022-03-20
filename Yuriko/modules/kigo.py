991702f047d7cb31c299013d4601a460ebeed3d7
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-20 21:03:52 +0530

    Create kigo.py

---
+++ Yuriko/modules/kigo.py
@@ -0,0 +1,14 @@
+__mod_name__ = "Pʜᴏɴᴇ"
+
+__help__ = """
+Commands
+
+✗ /phone - `to get phone details.`
+
+Ex💡 :- `/phone +91987655432📞`
+
+Note☢️ - `add your country code also`
+
+*✗ Pᴏᴡᴇʀᴇᴅ 🔥 Bʏ: Kɪɢᴏ Dᴜɴɪʏᴀ!*
+"""
+__lovely_tools__ = __help__
