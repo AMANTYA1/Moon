28f658b77247ba0207cd2cf15b095b81b763578b
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-26 08:53:53 +0530

    Update antiflood.py

--- Yuriko/modules/antiflood.py
+++ Yuriko/modules/antiflood.py
@@ -400,32 +400,6 @@ def __chat_settings__(chat_id, user_id):
         return "Antiflood has been set to`{}`.".format(limit)
 
 
-__help__ = """
-*Blue text cleaner* removed any made up commands that people send in your chat.
-  ✗ /cleanblue <on/off/yes/no>*:* clean commands after sending
-  ✗ /ignoreblue <word>*:* prevent auto cleaning of the command
-  ✗ /unignoreblue <word>*:* remove prevent auto cleaning of the command
-  ✗ /listblue*:* list currently whitelisted commands
-
- *Antiflood* allows you to take action on users that send more than x messages in a row. Exceeding the set flood \
- will result in restricting that user.
-  This will mute users if they send more than 10 messages in a row, bots are ignored.
-  ✗ /flood*:* Get the current flood control setting
- • *Admins only:*
-  ✗ /setflood <int/'no'/'off'>*:* enables or disables flood control
-  *Example:* `/setflood 10`
-  ✗ /setfloodmode <ban/kick/mute/tban/tmute> <value>*:* Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
- • *Note:*
-  • Value must be filled for tban and tmute!!
-  It can be:
-  `5m` = 5 minutes
-  `6h` = 6 hours
-   `3d` = 3 days
-    1w` = 1 week"""
-
-__lovely_basic__ = __help__
-__mod_name__ = "Fʟᴏᴏᴅ"
-
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.chat_type.groups, check_flood, run_async=True
 )
