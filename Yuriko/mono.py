b8039d4da61805b368eb69150d0c9f447cb6e058
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-05-25 16:48:36 +0530

    Update mono.py

--- Yuriko/mono.py
+++ Yuriko/mono.py
@@ -8,7 +8,7 @@ from Yuriko.confing import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
+MONGO_DB_URI = "mongodb+srv://Aman:Aman@cluster0.7bsvz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 MONGO_DB = "Yuriko"
 
 
@@ -16,7 +16,7 @@ client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["emiexrobot"]
+db = client["kigorobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
