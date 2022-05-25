07c2d797f4e54fe4b42fb024a3b083d6794bb9c4
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-05-26 00:59:31 +0530

    Update fakeit.py

--- Yuriko/modules/fakeit.py
+++ Yuriko/modules/fakeit.py
@@ -60,7 +60,7 @@ async def _(event):
             with open("FRIDAYOT.jpg", "wb") as f:
                 f.write(response.content)
 
-        captin = f"Fake Image powered by Null"
+        captin = f"Fake Image powered by @Kigo_omfobot"
         fole = "FRIDAYOT.jpg"
         await tbot.send_file(event.chat_id, fole, caption=captin)
         await event.delete()
