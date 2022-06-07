44afb043a7cb44391202e877da796aac9955a747
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-06-07 15:06:07 +0530

    Update google.py

--- Yuriko/modules/google.py
+++ Yuriko/modules/google.py
@@ -248,25 +248,15 @@ async def apk(e):
 __mod_name__ = "Sᴇᴀʀᴄʜ"
 
 __help__ = """
-✗ /google - `<query> Perform a google search`
-
 ✗ /image - `<query> Search Google for images and returns them\nFor greater no. of results specify lim, For eg:` `/img hello lim=10`
 
 ✗ /app - `<appname> Searches for an app in Play Store and returns its details.`
 
 ✗ /reverse - `Does a reverse image search of the media which it was replied to.`
 
-✗ /gps `<location> Get gps location.`
-
 ✗ /github - `<username> Get information about a GitHub user.`
 
-✗ /country - `<country name> Gathering info about given country`
-
 ✗ /imdb - `<Movie name> Get full info about a movie with imdb.com`
-
-✗ Yuriko - `<query> Yuriko answers the query`
-
-💡 Ex: `Kigo where is Japan?`
 
 *✗ Pᴏᴡᴇʀᴇᴅ 🔥 Bʏ: Kɪɢᴏ Dᴜɴɪʏᴀ!*
 """
