f5ec271d8d0452f5b2519f4e8dddcb44c5275c7b
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-06-09 01:16:38 +0530

    Create __logo.py

---
+++ Yuriko/modules/__logo.py
@@ -0,0 +1,27 @@
+from Yuriko.button.logo import *
+
+
+
+__help__ = """𖣘 Kɪɢᴏ Hᴀᴠᴇ Aʀᴇ Mᴀᴋɪɴɢ ᴛʜʀᴇᴇ Tʏᴘᴇs Lᴏɢᴏ 
+Hᴇʀᴇ ʏᴏᴜ sᴇᴇ.
+"""  # no help string
+
+__button__ = [ InlineKeyboardButton(text="Lᴏɢᴏ", callback_data="aliciafun_"),
+            InlineKeyboardButton(text="Sʟᴏɢᴋ", callback_data="aliciafunemoji_"),
+            InlineKeyboardButton(text="Gʟᴏɢᴏ", callback_data="aliciafungames_"),
+
+] 
+__buttons__ = [InlineKeyboardButton(text="Wʀɪᴛᴇ", callback_data="aliciafuncouple_"), 
+]
+
+
+
+__mod_name__ = "Lᴏɢᴏ"
+
+__lovely_basic__ = __help__
+
+dispatcher.add_handler(fun_callback_handler)
+dispatcher.add_handler(fun_emoji_callback_handler)
+dispatcher.add_handler(fun_games_callback_handler)
+dispatcher.add_handler(fun_couple_callback_handler)
+dispatcher.add_handler(fun_karma_callback_handler)
