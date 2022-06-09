b502e0e789e97e31c5af00d8ad4f50561b788de9
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-06-09 11:43:55 +0530

    Update logo.py

--- Yuriko/button/logo.py
+++ Yuriko/button/logo.py
@@ -12,13 +12,13 @@ from Yuriko.__main__ import *
 
 
 # Buttons Function for fun module
-LogoXd = """https://telegra.ph/file/c69a68148cf09661549f5.jpg"""
+
 
 def alicia_fun_callback(update, context):
     query = update.callback_query
     if query.data == "aliciafun_":
-        query.message.reply_photo(
-            Logoxd, caption=""" Here is the help for the *Logo* module:
+        query.message.edit_text(
+            text=""" Here is the help for the *Logo* module:
 
 ✗ /logo - `<text/name> Create a logo with your name`
 
@@ -36,7 +36,6 @@ def alicia_fun_callback(update, context):
                 ]
             ),
         )
-        query.message.delete()
 
 
 def alicia_fun_emoji_callback(update, context):
@@ -118,7 +117,6 @@ fun_callback_handler = CallbackQueryHandler(alicia_fun_callback, pattern=r"alici
 fun_emoji_callback_handler = CallbackQueryHandler(alicia_fun_emoji_callback, pattern=r"aliciafunemoji_", run_async=True)
 fun_games_callback_handler = CallbackQueryHandler(alicia_fun_games_callback, pattern=r"aliciafungames_", run_async=True)
 fun_couple_callback_handler = CallbackQueryHandler(alicia_fun_couple_callback, pattern=r"aliciafuncouple_", run_async=True)
-
 
 
 
