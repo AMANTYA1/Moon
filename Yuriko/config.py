5ead15c43ec0180e00a11ef22471e796115a8f3e
NullCoder <95975318+AMANTYA1@users.noreply.github.com>
2023-04-17 14:55:03 +0530

    Update config.py

--- Yuriko/config.py
+++ Yuriko/config.py
@@ -16,7 +16,7 @@ class Config(object):
 
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
-    TOKEN = "5762587912:AAHsGPWxcML1xnbAECXkuWQ9OIawa22NnXE"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    TOKEN = "6257271972:AAH2jZ_JhrZTY755yRu44xpOoFfPjpbIHFU"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 5817544006  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "Nullcrow"
     SUPPORT_CHAT = "VILLA_HERE"  # Your own group for support, do not add the @
